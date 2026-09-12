ab4981d2108fef92fefc4c060a1aea576202b50d
Alaa Wahba <alaawahbaa13@gmail.com>
2026-09-12 22:34:26 +0300

    fix: icon

--- aya-and-farhat-wedding/app/layout.tsx
+++ aya-and-farhat-wedding/app/layout.tsx
@@ -34,20 +34,12 @@ export const metadata: Metadata = {
   generator: 'v0.app',
   icons: {
     icon: [
-      // {
-      //   url: '/icon-light-32x32.png',
-      //   media: '(prefers-color-scheme: light)',
-      // },
-      // {
-      //   url: '/icon-dark-32x32.png',
-      //   media: '(prefers-color-scheme: dark)',
-      // },
       {
         url: '/icon.png',
-        type: 'image/svg+xml',
+        type: 'image/png',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/icon.png',
   },
 }
 
